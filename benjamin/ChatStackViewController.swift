06c6da91a5957ed109ddc8b60af8806db5546b2f
Brandon Lau <brandonlau2@gmail.com>
2017-08-22 23:42:49 -0700

    Resize view when keyboard appears

--- benjamin/ChatStackViewController.swift
+++ benjamin/ChatStackViewController.swift
@@ -9,16 +9,22 @@
 import Foundation
 import UIKit
 
-class ChatStackViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ChatStackViewController: UIViewController, UITableViewDataSource, UITextViewDelegate {
     
     struct Constants {
         static let cellReuseIdentifier = "ChatTableViewCell"
+        static let placeholderText = "Type a message..."
+        
     }
     
     @IBOutlet weak var chatStackView: UIStackView!
     @IBOutlet weak var leaderboardView: UIView!
     @IBOutlet weak var chatTableView: UITableView!
-    @IBOutlet weak var textInputView: UIView!
+    @IBOutlet weak var chatInputView: UIView!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var sendButton: UIButton!
+    
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     
     var messages: [String]
     
@@ -27,12 +33,22 @@ class ChatStackViewController: UIViewController, UITableViewDataSource, UITableV
         super.init(coder: aDecoder)
     }
     
-    override func viewDidLoad() {
-        chatTableView.dataSource = self
-        chatTableView.delegate = self
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
-    // MARK: UITableViewDataSource delegate methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardNotification(notification:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+        textView.textColor = UIColor.lightGray
+        textView.text = Constants.placeholderText
+        sendButton.isEnabled = false
+        sendButton.setTitleColor(.lightGray, for: .disabled)
+        chatTableView.dataSource = self
+        textView.delegate = self
+    }
+    
+    // MARK: UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return messages.count
     }
@@ -43,4 +59,36 @@ class ChatStackViewController: UIViewController, UITableViewDataSource, UITableV
         return cell
     }
     
+    // MARK: UITextViewDelegate
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        if textView.text.characters.count > 0 {
+            sendButton.isEnabled = true
+        } else {
+            sendButton.isEnabled = false
+        }
+    }
+    
+    // MARK: NSNotification
+    
+    @objc func keyboardNotification(notification: NSNotification) {
+        guard let userInfo = notification.userInfo,
+            let endFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
+            let duration = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue else {
+            return
+        }
+        let animationCurve = UIViewAnimationOptions.curveEaseInOut
+        if endFrame.origin.y >= UIScreen.main.bounds.size.height {
+            bottomConstraint.constant = 0.0
+        } else {
+            bottomConstraint.constant = endFrame.size.height
+        }
+        UIView.animate(withDuration: duration, delay: 0.0, options: animationCurve, animations: { self.view.layoutIfNeeded() }, completion: nil)
+    }
 }
